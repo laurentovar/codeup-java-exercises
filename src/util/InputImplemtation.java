82940f361e70f9ed58fc3d4dacc9e951c8e5dea5
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-30 22:44:31 -0600

    input ex

--- src/util/InputImplemtation.java
+++ src/util/InputImplemtation.java
@@ -1,28 +1,43 @@
+//package util;
+//import java.util.Scanner;
+//
+//public class InputImplemtation {
+//    public static void main(String[] args) {
+//        Input myInput = new Input();
+//
+//        Scanner userInput = new Scanner(System.in);
+//        System.out.println("Enter a random sentence");
+//        String randomSentence = userInput.nextLine();
+//
+//
+//        userInput.nextLine();
+//        System.out.println("Enter yes or no");
+//        String yesNoanswer = userInput.nextLine();
+//
+//
+//        Boolean yesNo = yesNoanswer.equalsIgnoreCase("Y") || yesNoanswer.equalsIgnoreCase("yes") ? true : false;
+//        System.out.println(yesNo);
+//
+//
+//
+//
+//
+//
+//
+//    }
+//}
 package util;
-import java.util.Scanner;
 
-public class InputImplemtation {
+public class InputImplemtation  {
     public static void main(String[] args) {
-        Input myInput = new Input();
+        Input input = new Input();
 
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter a random sentence");
-        String randomSentence = userInput.nextLine();
+        System.out.println("What's your name");
+        System.out.println(input.getString());
+        System.out.println(input.getInt(1, 10));
 
+        System.out.println(input.getDouble(1, 10));
 
-        userInput.nextLine();
-        System.out.println("Enter yes or no");
-        String yesNoanswer = userInput.nextLine();
-
-
-        Boolean yesNo = yesNoanswer.equalsIgnoreCase("Y") || yesNoanswer.equalsIgnoreCase("yes") ? true : false;
-        System.out.println(yesNo);
-
-
-
-
-
-
-
+        System.out.println(input.yesNo());
     }
 }

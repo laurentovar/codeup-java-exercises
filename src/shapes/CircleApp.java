2fa41c2fb1acabb02bd1586df01344175b5714bc
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-30 22:50:44 -0600

    circle app

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,13 @@
+
 package shapes;
 
+import util.Input;
+
 public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        Circle circle = new Circle(input.getDouble());
+        System.out.println("circle.getArea() = " + circle.getArea());
+        System.out.println("circle.getCircumference() = " + circle.getCircumference());
+    }
 }

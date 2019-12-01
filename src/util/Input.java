4a4bf22aca2d1169370725e3cebc46b837ec3a90
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-30 22:43:48 -0600

    input

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,29 +1,83 @@
+//package util;
+//import java.util.Scanner;
+//public class Input {
+//    private Scanner userInput = new Scanner(System.in);
+//    public String yesNoAnswer;
+//
+//
+//    public String getString() {
+//        return ("Your string");
+//    }
+//    public  Boolean yesNo (){
+//        return true;
+//    }
+//    public int getInt(int min, int max) {
+//        return 1;
+//    }
+//    public int getInt(){
+//        return 1;
+//    }
+//    double getDouble (double min, double max){
+//        return 1;
+//    }
+//    double getDouble(){
+//        return 1;
+//    }
+//
+//
+//}
+
 package util;
-import java.util.Scanner;
+
+        import java.util.Scanner;
+
 public class Input {
-    private Scanner userInput = new Scanner(System.in);
-    public String yesNoAnswer;
 
+    private Scanner scanner = new Scanner(System.in);
 
-    public String getString() {
-        return ("Your string");
+    public String getString(){
+        return scanner.nextLine();
     }
-    public  Boolean yesNo (){
-        return true;
+
+    public boolean yesNo(){
+        System.out.println("Type: yes/y");
+        String input = getString();
+        return input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y");
     }
-    public int getInt(int min, int max) {
-        return 1;
+
+    public int getInt(int min, int max){
+
+        int number = getInt();
+
+        if(number < min){
+            return getInt(min, max);
+        } else if (number > max) {
+            return getInt(min, max);
+        }
+
+        return number;
     }
+
     public int getInt(){
-        return 1;
-    }
-    double getDouble (double min, double max){
-        return 1;
-    }
-    double getDouble(){
-        return 1;
+        System.out.println("Give me a number");
+        return Integer.parseInt(getString());
     }
 
+    public double getDouble(double min, double max){
+        double number = getDouble();
+
+        if(number < min){
+            return getDouble(min, max);
+        } else if (number > max) {
+            return getDouble(min, max);
+        }
+
+        return number;
+    }
+
+    public double getDouble(){
+        System.out.println("Give me a decimal");
+        return Double.parseDouble(getString());
+    }
 
 }
-

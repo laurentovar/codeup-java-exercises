9be95edb16f256550ffa8301e572e008788175a3
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-01 19:34:47 -0600

    input

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,31 +1,3 @@
-//package util;
-//import java.util.Scanner;
-//public class Input {
-//    private Scanner userInput = new Scanner(System.in);
-//    public String yesNoAnswer;
-//
-//
-//    public String getString() {
-//        return ("Your string");
-//    }
-//    public  Boolean yesNo (){
-//        return true;
-//    }
-//    public int getInt(int min, int max) {
-//        return 1;
-//    }
-//    public int getInt(){
-//        return 1;
-//    }
-//    double getDouble (double min, double max){
-//        return 1;
-//    }
-//    double getDouble(){
-//        return 1;
-//    }
-//
-//
-//}
 
 package util;
 

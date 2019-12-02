73ef165ec24ace13f6b7eded14d194b602f622d7
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-01 19:35:16 -0600

    input implemtation

--- src/util/InputImplemtation.java
+++ src/util/InputImplemtation.java
@@ -1,31 +1,3 @@
-//package util;
-//import java.util.Scanner;
-//
-//public class InputImplemtation {
-//    public static void main(String[] args) {
-//        Input myInput = new Input();
-//
-//        Scanner userInput = new Scanner(System.in);
-//        System.out.println("Enter a random sentence");
-//        String randomSentence = userInput.nextLine();
-//
-//
-//        userInput.nextLine();
-//        System.out.println("Enter yes or no");
-//        String yesNoanswer = userInput.nextLine();
-//
-//
-//        Boolean yesNo = yesNoanswer.equalsIgnoreCase("Y") || yesNoanswer.equalsIgnoreCase("yes") ? true : false;
-//        System.out.println(yesNo);
-//
-//
-//
-//
-//
-//
-//
-//    }
-//}
 package util;
 
 public class InputImplemtation  {

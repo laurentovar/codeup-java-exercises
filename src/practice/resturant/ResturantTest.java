869fb22a2b48852ec07dc9556628581797379a28
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-11 13:23:46 -0600

    short assignment resturant 1 completed

--- src/practice/resturant/ResturantTest.java
+++ src/practice/resturant/ResturantTest.java
@@ -4,9 +4,10 @@ public class ResturantTest {
     public static void main(String[] args) {
         //instantiate a dish called dish
 
-        RestaurantDish dish = new RestaurantDish("dish" ,10, false);
+        RestaurantDish dish = new RestaurantDish("Cheesy bread sticks" ,10, true);
 
-        System.out.println("Your dish is: " + dish.getNameOfDish() + " " + "the cost $ " + dish.getCost() + " " + " \nI would recommend: " + dish.getWouldRecommend());
-
+        System.out.println("Your dish is: " + dish.getNameOfDish()+"\n"  + "Dish cost $ " + dish.getCost() + " " + " \nI would recommend: " + dish.getWouldRecommend());
+        //call them eating the dish
+        dish.eat();
     }
 }

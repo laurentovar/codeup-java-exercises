490dd5f5f9113b2e5f45955aa3dc811a24f54831
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-11 18:13:51 -0600

    added mini exceptions example

---
+++ src/practice/exceptions/ExeceptionsEx.java
@@ -0,0 +1,11 @@
+package practice.exceptions;
+
+public class ExeceptionsEx {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if(x == 5){
+            throw  new ArithmeticException();
+        }
+    }
+}

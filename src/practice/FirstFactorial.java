3dfc628774107a7ec2bce8987ed6607df90af80b
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-19 09:12:49 -0600

    added practice firstfactorial

---
+++ src/practice/FirstFactorial.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class FirstFactorial {
+    public static int factorial (int num){
+        int result = 1;
+
+        for (int i=2; i <=num; i++ ){
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+}

a29e112be0e94751e6716f3552d32c021d7eef8e
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-12-20 10:03:27 -0600

    shortest string added

--- src/laurenWeek2/Shortest.java
+++ src/laurenWeek2/Shortest.java
@@ -1,36 +1,52 @@
 package laurenWeek2;
+
 //repeats the shortest string until its equal to the length of the longer string
-
 public class Shortest {
-    public static String lengthen(String str1, String str2) {
-        String shorter = shorterString(str1, str2);
-        String longer = longerString(str1, str2);
-        String result = "";
+    public static String lengthenStr(String str1, String str2) {
 
-        for (int i= 0; i<(longer.length()/shorter.length()); i++) {
-            result += shorter;
+        //Make shortStr = to the shorterString Method
+        String shortStr = shorterString(str1, str2);
+        //Make longStr = to the longerString Method
+        String longStr = longerString(str1, str2);
+
+        String strResult = "";
+
+        for (int i= 0; i < (longStr.length() / shortStr.length()); i++) {
+
+            //puts the shortStr into the empty result
+            strResult += shortStr;
         }
 
-        result += shorter.substring(0, longer.length() % result.length());
+        //start at beginning of shortStr and go to the longStr length
+        strResult += shortStr.substring(0, longStr.length() % strResult.length());
 
-        return result;
+        System.out.println("Short str length: " + shortStr.length() + " Long str length: " + longStr.length());
+        return strResult;
     }
 
+    //method for shortStr
     public static String shorterString(String str1, String str2) {
         if (str1.length() < str2.length()) {
             return str1;
-        } else return str2;
+        }
+        else {
+            return str2;
+        }
     }
 
+    //method for longStr
     public static String longerString(String str1, String str2) {
         if (str1.length() > str2.length()) {
             return str1;
-        } else return str2;
+        }
+        else {
+            return str2;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(lengthen("hellllloooo", "hello"));
-        System.out.println(lengthen("coffee", "starbucks"));
-        System.out.println(lengthen("buck", "buckero"));
+        System.out.println(lengthenStr("christmas", "holiday"));//should return holidayho
+        System.out.println(lengthenStr("coffee", "starbucks"));//should return coffeecof
+        System.out.println(lengthenStr("buck", "buckero"));//should return buckbuc
     }
 }

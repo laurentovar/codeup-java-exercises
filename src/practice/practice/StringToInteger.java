9fde6be8b3e6ea4132037cf2e9337e0a7b925f83
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2020-01-13 09:24:04 -0600

    string to int practice

---
+++ src/practice/practice/StringToInteger.java
@@ -0,0 +1,15 @@
+package practice.practice;
+//convert a string to an integer
+
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        String stringInt = "2014";
+
+        //convert it
+        int i = Integer.parseInt(stringInt);
+
+        System.out.println(i);
+    }
+
+}
